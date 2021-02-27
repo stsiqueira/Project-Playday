4d74103ad2259310e3efda5d83ae21eda32170b3
glen thomas <glenkt2@gmail.com>
2021-02-27 06:04:26 -0800

    pwa added

---
+++ js/login.js
@@ -0,0 +1,86 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyATN0KrGH8WPsgEiozxcev0W3Fgq3F41i4",
+    authDomain: "test-1-5be12.firebaseapp.com",
+    projectId: "test-1-5be12",
+    storageBucket: "test-1-5be12.appspot.com",
+    messagingSenderId: "419371280336",
+    appId: "1:419371280336:web:e6a5055469d95d9d777bd5",
+    measurementId: "G-DP5FXPH4WE"
+};
+    // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const mailField = document.getElementById('mail');
+const passwordField = document.getElementById('password');
+const signInWithMail = document.getElementById('signInWithMail');
+const signUp = document.getElementById('signUp');
+const googlesignin = document.getElementById("googlesignup")
+
+var db = firebase.firestore();
+
+//Sign in function
+const signInWithEmailFunction = () => {
+	const email = mailField.value;
+	const password = passwordField.value;
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.then((userCredential) => {
+			// Signed in 
+			var user = userCredential.user;
+	        window.location.assign('../html/loggedin.html');
+
+			console.log(user);
+			// db.collection("users").doc(user.uid).set({
+			// 	CourtID: "",
+			// 	Ranking: "Beginner",
+			// 	name: user.displayName,
+			// 	userID: user.uid,
+			// 	profilepic: user.photoURL
+			// })
+			// .then((docRef) => {
+			// 	console.log("Document written with ID: ");
+			// })
+			// .catch((error) => {
+			// 	console.error("Error adding document: ", error);
+			// });
+		})
+		.catch(error => {
+			console.error(error);
+		})
+}
+
+signInWithMail.addEventListener('click', signInWithEmailFunction);
+
+signUp.addEventListener('click', () => {
+	window.location.assign('signup.html');
+});
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+// ui.start('#firebaseui-auth-container', uiConfig);
+
+document.getElementById("find-me").addEventListener("click", function() {
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+
+    mapLink.href = '';
+    mapLink.textContent = '';
+
+    function success(position) {
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        status.textContent = '';
+        mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+
+    function error() {
+        status.textContent = 'Unable to retrieve your location';
+        }
+
+        if(!navigator.geolocation) {
+        status.textContent = 'Geolocation is not supported by your browser';
+        } else {
+        status.textContent = 'Locating…';
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+});
