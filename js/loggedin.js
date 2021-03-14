f064be8aeac1f8bd1a4b3d1fb0f38e5f7b52fd67
factorcode <77560128+factorcode@users.noreply.github.com>
2021-03-14 15:48:45 -0700

    Sprint 1.2

--- js/loggedin.js
+++ js/loggedin.js
@@ -1,31 +1,34 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if(user) {
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
         var div = document.getElementById('firebaseui-auth-container');
         var db = firebase.firestore();
         db.collection("user").where("userID", "==", user.uid)
-        .get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.data());
-                console.log(doc.data().name);
-                var username = doc.data().name;
-                localStorage.setItem("username", username);
-                div.innerHTML = `<h2> Congrats ${username} logged in with ${user.email}</h2>`;
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.data());
+                    console.log(doc.data().name);
+                    var username = doc.data().name;
+
+                    let au = new AppUser(doc.data().userID, doc.data().name.substring(0, doc.data().name.indexOf(" ")), doc.data().name.substring(doc.data().name.indexOf(" ")+1, doc.data().name.length),doc.data().dateOfBirth, doc.data().profilePic, doc.data().about, doc.data().userLocation, doc.data().sports);
+                    set_appUser(au);
+
+                    div.innerHTML = `<h2> Congrats ${username} logged in with ${user.email}</h2>`;
+                });
+            })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
             });
-        })
-        .catch((error) => {
-            console.log("Error getting documents: ", error);
-        });
     }
     else {
         window.location = "../index.html";
     }
 });
 
-function signout(){
+function signout() {
     firebase.auth().signOut();
-    window.location='../index.html';
+    window.location = '../index.html';
 }
 
 
