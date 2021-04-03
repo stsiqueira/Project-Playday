9467551fb90c4947f1a439409273b4a235fe963e
factorcode <77560128+factorcode@users.noreply.github.com>
2021-04-03 01:18:09 -0700

    Console error Fixes

--- js/challenge-invite.js
+++ js/challenge-invite.js
@@ -1,24 +1,24 @@
 ////////////////////////////////////////////
 //  Variables
 //////////////////////////////////////////// 
-var firebaseConfig = {
-    apiKey: "AIzaSyCVfkLdpaLZUwFN8eMVMSptFoZfOpp1pZ8",
-    authDomain: "playday-f43e6.firebaseapp.com",
-    databaseURL: "https://playday-f43e6-default-rtdb.firebaseio.com",
-    storageBucket: "https://console.firebase.google.com/project/playday-f43e6/storage/playday-f43e6.appspot.com/files",
-    projectId: "playday-f43e6",
-    storageBucket: "playday-f43e6.appspot.com",
-    messagingSenderId: "732773100147",
-    appId: "1:732773100147:web:13f7a6804851ac8486d806",
-    measurementId: "G-TZB3NY5S6W"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCVfkLdpaLZUwFN8eMVMSptFoZfOpp1pZ8",
+//     authDomain: "playday-f43e6.firebaseapp.com",
+//     databaseURL: "https://playday-f43e6-default-rtdb.firebaseio.com",
+//     storageBucket: "https://console.firebase.google.com/project/playday-f43e6/storage/playday-f43e6.appspot.com/files",
+//     projectId: "playday-f43e6",
+//     storageBucket: "playday-f43e6.appspot.com",
+//     messagingSenderId: "732773100147",
+//     appId: "1:732773100147:web:13f7a6804851ac8486d806",
+//     measurementId: "G-TZB3NY5S6W"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 ////////////////////////////////////////////
 //  Variables
 //////////////////////////////////////////// 
 
-const db =firebase.firestore();
+// let db =firebase.firestore();
 let friendProfile = {}; 
 let userAppProfile = {}; 
 let verifyChallenge = false;
