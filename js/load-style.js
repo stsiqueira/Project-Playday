3ebbcf4dea870712a5a821642754a5723636cfd5
factorcode <77560128+factorcode@users.noreply.github.com>
2021-04-11 20:23:43 -0700

    Location Selection Branding

--- js/load-style.js
+++ js/load-style.js
@@ -4,6 +4,7 @@ var head = document.getElementsByTagName('HEAD')[0];
 // Create new link Element
 var link = document.createElement('link');
 var versionUpdate = (new Date()).getTime(); 
+// var versionUpdate = 1.1; 
 // set the attributes for link element 
 link.rel = 'stylesheet'; 
 link.type = 'text/css';
