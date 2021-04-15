e919d963c852e479c8a274a45342205a02e02823
glen thomas <glenkt2@gmail.com>
2021-04-15 09:57:55 -0700

    sw.js updated

--- sw.js
+++ sw.js
@@ -9,11 +9,13 @@ const assets = [
 	"./html/articles/tennis-article.html",
 	"./html/articles/volleyball-article.html",
 	"./js/home.js",
+	"./js/load-style.js",
 	"./js/common.js",
 	"./img/logo.png",
 	"./img/home-screen/hero-img.png",
 	"./img/home-screen/tennis-main.png",
 	"./img/home-screen/badminton-main.png",
+	"./img/home-screen/volleyball-main.png",
 	"./img/articles-section/volleyball-1.jpg",
 	"./img/articles-section/volleyball-2.jpg",
 	"./img/articles-section/volleyball-3.jpg",
@@ -27,13 +29,12 @@ const assets = [
 	"./img/articles-section/badminton-3.jpg",
 	"./img/articles-section/badminton-hero-image.jpg",
 	"https://kit.fontawesome.com/ef8606d888.js",
+	"favicon.ico",
 	"https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
 ];
 
-
 // install event
 self.addEventListener('install', evt => {
-	//console.log('service worker installed');
 	evt.waitUntil(
 		caches.open(staticCacheName).then((cache) => {
 			console.log('caching shell assets');
@@ -60,11 +61,11 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
 	evt.respondWith(
 		caches.match(evt.request).then(cacheRes => {
-		  return cacheRes || fetch(evt.request)
+		return cacheRes || fetch(evt.request)
 		}).catch(() => {
-			if(evt.request.url.indexOf('.html') > -1){
-			  return caches.match("./html/offline.html");
-			}
+			return caches.match("./html/offline.html");
 		})
+	)	 
   	)	 
+	)	 
 });
