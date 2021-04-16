34984f6cdc115a8f8d1f79127eae91404799f45c
glen thomas <glenkt2@gmail.com>
2021-04-15 18:28:04 -0700

    changed the cdn for font awesome

--- sw.js
+++ sw.js
@@ -1,16 +1,11 @@
 const staticCacheName = 'site-static-v2';
 const assets = [
-	"/",
 	"./index.html",
 	"./style.css",
-	"./html/home.html",
 	"./html/offline.html",
 	"./html/articles/badminton-article.html",
 	"./html/articles/tennis-article.html",
 	"./html/articles/volleyball-article.html",
-	"./js/home.js",
-	"./js/load-style.js",
-	"./js/common.js",
 	"./img/logo.png",
 	"./img/home-screen/hero-img.png",
 	"./img/home-screen/tennis-main.png",
@@ -28,17 +23,19 @@ const assets = [
 	"./img/articles-section/badminton-2.jpg",
 	"./img/articles-section/badminton-3.jpg",
 	"./img/articles-section/badminton-hero-image.jpg",
-	"https://kit.fontawesome.com/ef8606d888.js",
-	"favicon.ico",
-	"https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+	"./js/common.js",
+	"./favicon.ico",
+	"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js",
+	"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+	"https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 ];
 
+
 // install event
-self.addEventListener('install', evt => {
-	evt.waitUntil(
-		caches.open(staticCacheName).then((cache) => {
-			console.log('caching shell assets');
-			cache.addAll(assets);
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache) {
+		return cache.addAll(assets);
 		})
 	);
 });
@@ -61,11 +58,9 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
 	evt.respondWith(
 		caches.match(evt.request).then(cacheRes => {
-		return cacheRes || fetch(evt.request)
+		  return cacheRes || fetch(evt.request)
 		}).catch(() => {
 			return caches.match("./html/offline.html");
 		})
-	)	 
-  	)	 
-	)	 
+	);
 });
